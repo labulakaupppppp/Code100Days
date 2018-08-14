7e08ce7d185a0ea0a88cf092cd70b3da4038ead4
labulakaupppppp <1209011672@qq.com>
2018-08-14 10:57:05 +0800

    剑指offer29 求数组中大于半数的数字

---
+++ src/newCoder/MoreThanHalfNum.java
@@ -0,0 +1,48 @@
+package newCoder;
+
+public class MoreThanHalfNum {
+	/**
+	 * 剑指offer 29题
+	 * @param nums
+	 * @return 数组中超过一半的数字
+	 */
+	public static int more_than_half_num(int[] nums) {
+		int key = nums[0];
+		int count = 1;
+		for (int i = 0; i < nums.length; i++) {
+			if (count == 0) {
+				key = nums[i];
+				count = 1;
+			}
+			if (nums[i] == key) {
+				count++;
+			} else {
+				count--;
+			}
+		}
+		boolean ismore=isMoreThanHalf(nums,key);
+		if(ismore==true)
+			return key;
+		else 
+			return 0;
+	}
+public static boolean  isMoreThanHalf(int[] nums, int key) {
+	int count=0;
+	boolean ismore=false;
+	for(int i=0;i<nums.length;i++) {
+		if(nums[i]==key) {
+			count++;
+		}
+	}
+	if(count>=nums.length/2+1) {
+		ismore=true;
+		}
+	return ismore;
+}
+	public static void main(String[] args) {
+		int[] a = {  2, 5, 2, 6, 2, 7, 2, 2 };
+		int [] b= {1,2,3};
+		int ans = more_than_half_num(b);
+		System.out.println(ans);
+	}
+}
