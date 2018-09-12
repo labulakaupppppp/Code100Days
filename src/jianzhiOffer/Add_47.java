cc2b335cfbeadb67570b051111e9d2c21ec486c8
labulakaupppppp <1209011672@qq.com>
2018-09-12 10:35:43 +0800

    47 不用减加乘除进行加法运算

---
+++ src/jianzhiOffer/Add_47.java
@@ -0,0 +1,24 @@
+package jianzhiOffer;
+
+public class Add_47 {
+	//不使用 加减乘除计算加法
+	/**
+	 * 例如：5+7（101+111）
+	 * num1^num2:010
+	 * (num1 & num2) << 1:1010
+	 *  num1^num2:1000
+	 * (num1 & num2) << 1=(010&1010)<<1:100
+	 * num1^num2=1000^100 = 1100，
+	 * (num1 & num2) << 1= (1000^100)<<1=0进位值为0
+	 * 结束
+	 */
+	public int Add(int num1, int num2) {
+		while (num2 != 0) {
+			int temp = num1 ^ num2;
+			num1 = (num1 & num2) << 1;
+			num2 = temp;
+		}
+		return num1;
+
+	}
+}
