8ab5b4790283b1cf0d2ef40c980a2edea7a7999f
labulakaupppppp <1209011672@qq.com>
2018-09-12 10:44:30 +0800

    47 不用减加乘除进行加法运算

--- src/jianzhiOffer/Add_47.java
+++ src/jianzhiOffer/Add_47.java
@@ -15,8 +15,8 @@ public class Add_47 {
 	public int Add(int num1, int num2) {
 		while (num2 != 0) {
 			int temp = num1 ^ num2;
-			num1 = (num1 & num2) << 1;
-			num2 = temp;
+			num2 = (num1 & num2) << 1;
+			num1 = temp;
 		}
 		return num1;
 
