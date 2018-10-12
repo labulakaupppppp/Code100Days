f4e597cfc6677553c1e725ba3b7ca597775c1456
labulakaupppppp <1209011672@qq.com>
2018-10-12 18:03:07 +0800

    序列化 pi

--- src/jianzhiOffer/Serialize_BST.java
+++ src/jianzhiOffer/Serialize_BST.java
@@ -9,56 +9,77 @@ public class Serialize_BST {
 	String Serialize1(TreeNode root) {
 		if (root == null)
 			return "";
-		String ans = "";
+		String ans = "{";
 		Queue<TreeNode> que = new LinkedList<>();
 		que.add(root);
 		while (!que.isEmpty()) {
-			
 			TreeNode node = que.poll();
 			if (node != null) {
 				ans += node.val;
-				ans += " ";
+				ans += ",";
 				que.add(node.left);
 				que.add(node.right);
 			} else {
-				ans += "* ";
+				ans += "#,";
 			}
-
 		}
-		return ans;
-
+		return ans + "}";
 	}
+
 	String Serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        if(root == null){
-            sb.append("* ");
-            return sb.toString();
-        }
-        sb.append(root.val + " ");
-        sb.append(Serialize(root.left));
-        sb.append(Serialize(root.right));
-        return sb.toString();
-  }
-  
-	static int index = -1;
-
-	TreeNode Deserialize(String str) {
-		index++;
-		if (str == null && str.length() == 0)
-			return null;
-		String[] ch = str.split(" ");
-		//System.out.println(Arrays.toString(ch));
-		TreeNode root = null;
-		while (!ch[index].equals("*")) {
-			root = new TreeNode(Integer.valueOf(ch[index]));
-			root.left = Deserialize(str);
-			root.right = Deserialize(str);
+		StringBuilder sb = new StringBuilder();
+		if (root == null) {
+			sb.append("#,");
+			return sb.toString();
 		}
-
-		return root;
-
+		sb.append(root.val + ",");
+		sb.append(Serialize(root.left));
+		sb.append(Serialize(root.right));
+		return sb.toString();
 	}
 
+	 int index = -1;
+	 TreeNode Deserialize1(String str) {
+	        index++;
+	        //int len = str.length();
+	        //if(index >= len){
+	        //    return null;
+	       // }
+	        String[] strr = str.split(",");
+	        TreeNode node = null;
+	        if(!strr[index].equals("#")){
+	            node = new TreeNode(Integer.valueOf(strr[index]));
+	            node.left = Deserialize1(str);
+	            node.right = Deserialize1(str);
+	        }
+	        return node;
+	  }
+	
+	   //正确
+	    String Serialize2(TreeNode root) {
+	        StringBuilder sb = new StringBuilder();
+	        if(root == null){
+	            sb.append("#,");
+	            return sb.toString();
+	        }
+	        sb.append(root.val + ",");
+	        sb.append(Serialize2(root.left));
+	        sb.append(Serialize2(root.right));
+	        return sb.toString();
+	  }
+	    TreeNode Deserialize2(String str) {
+	        index++;
+	        String[] strr = str.split(",");
+	        System.out.println(Arrays.toString(strr));
+	        TreeNode node = null;
+	        if(index==strr.length) return null;
+	        if(!strr[index].equals("#")){
+	            node = new TreeNode(Integer.valueOf(strr[index]));
+	            node.left = Deserialize2(str);
+	            node.right = Deserialize2(str);
+	        }
+	        return node;
+	  }
 	public static void main(String[] args) {
 		Serialize_BST s1 = new Serialize_BST();
 		TreeNode root = new TreeNode(1);
@@ -68,7 +89,7 @@ public class Serialize_BST {
 		root.right = root2;
 		root2.left = null;
 		root2.right = null;
-		String ans = s1.Serialize(root);
+		 String ans = s1.Serialize2(root);
 		System.out.println(ans);
 		// 1 2 * **
 		TreeNode node = new TreeNode(1);
@@ -79,10 +100,10 @@ public class Serialize_BST {
 		node.right = node2;
 		node1.left = null;
 		node1.right = null;
-		
-		System.out.println("!!!!!!!1");
-		TreeNode nnn=s1.Deserialize("8 6 10 5 7 9 11");
-		String ans1 = s1.Serialize1(nnn);
+
+		System.out.println("!!!!!!!");
+		TreeNode nnn = s1.Deserialize2("8,6,10,5,7,9,11");
+		String ans1 = s1.Serialize2(nnn);
 		System.out.println(ans1);
 	}
 
